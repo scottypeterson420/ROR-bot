2c0cf3337f7ee5513441b0c51cc992b865d06109
ROR341 <Cecillester721@outlook.com>
2022-03-27 19:49:42 +0200

    Update command execute method

--- lib/copy_bot/command.rb
+++ lib/copy_bot/command.rb
@@ -8,7 +8,7 @@ module CopyBot
     # @return [FalseClass, TrueClass]
     def execute
       stdout, stderr, status = Open3.capture3(command_string)
-      return true if status.exitstatus&.zero? && stderr.blank?
+      return true if status.exitstatus&.zero?
 
       CopyBot.config.logger.debug { "#{stdout}, #{stderr}" }
       false
